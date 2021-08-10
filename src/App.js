f5e2890b213df5618c9df7307a23bce7e1b5bdb1
naman jain <naman2001jain@gmail.com>
2021-08-10 11:54:17 +0530

    introduction to state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import Todo from './components/Todo'
-import Modal from './components/Modal';
-import Backdrop from './components/Backdrop';
 function App() {
   return <div>
     <h1>My Todos</h1>
     <Todo title="learn react"/>
     <Todo title="learn dsa"/>
     <Todo title="learn ios"/>
-    <Modal />
-    <Backdrop />
+    
   </div>;
 }
 
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,25 @@
+import { useState } from 'react';
+import Modal from './Modal';
+import Backdrop from './Backdrop';
+function Todo(props) {
 
-function Todo(props){
-    return (<div className="card">
-      <h2>{props.title}</h2>
-      <button className="btn">Delete</button>
-    </div>);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  function deleteHandler() {
+    setModalIsOpen(true);
+  }
+  function closeHandler(){
+    setModalIsOpen(false);
+  }
+  
+  return (<div className="card">
+    <h2>{props.title}</h2>
+    <div className="actions">
+      <button className="btn" onClick={deleteHandler}>Delete</button>
+    </div>
+    { modalIsOpen ? <Modal onCancel={closeHandler} onConfirm={closeHandler}/> : null}
+    {modalIsOpen? <Backdrop onClose={closeHandler}/>: null}
+  </div>);
+
 }
 
 export default Todo;
